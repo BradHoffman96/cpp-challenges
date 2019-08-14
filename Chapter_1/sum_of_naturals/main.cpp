5d9fc383785abdd9ce0c2ad9516634f02b7129f2
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-14 11:00:07 +0000

    summed all of the numbers. need to use an unsigned long long to really get the largest number possible

--- Chapter_1/sum_of_naturals/main.cpp
+++ Chapter_1/sum_of_naturals/main.cpp
@@ -3,6 +3,8 @@
 // Write a program that calculates and prints the sum of all the natural numbers divisble by either 3 or 5,
 // Up to a given limit entered by the user.
 
+using namespace std;
+
 int main() {
   
   std::cout << "This program will find all numbers divisible by 3 and 5 up to a limit.\n";
@@ -11,10 +13,13 @@ int main() {
   int limit;
   std::cin >> limit;
 
+  unsigned long long sum = 0;
   for (int i = 1; i <= limit; i++) {
     if (i % 3 == 0 || i % 5 == 0)
-      std::cout << i << '\n';
+      sum += i;
   }
+
+  cout << "SUM: " << sum << '\n';
 
   return 0;
 }
